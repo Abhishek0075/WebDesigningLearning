508c644c2089f201027e86c6e0c2f1543a9ec13a
abhirajp075@gmail.com <abhirajp075@gmail.com>
2022-10-15 21:31:15 +0530

    new day and new changes

--- bootcamp/day6/script.js
+++ bootcamp/day6/script.js
@@ -1,6 +1,20 @@
+var isClicked=false;
 function buttonClick(val){
+    if(isClicked==true){
+        document.getElementById("screen").value="";
+    }
     document.getElementById("screen").value+=val;
 }
-function displayScreen(){
+
+function clearDisplay(){
     document.getElementById("screen").value="";
+}
+
+function equalClicked(){
+    var text=document.getElementById("screen").value;
+    var result=eval(text);
+    console.log("Result = "+result);
+    document.getElementById("screen").value=result;
+    isClicked=true;
+
 }
